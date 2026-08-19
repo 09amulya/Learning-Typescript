f11cb32202b350df6eb880e8ad72ca14b2b67dee
Amulya <singhamulya09@gmail.com>
2026-08-19 23:43:06 +0530

    types,union and  any  in ts

---
+++ src/Typesints.ts
@@ -0,0 +1,11 @@
+let drink ="chai"
+let cups = Math.random() >0.5 ? 10:5;
+
+let channelName = "Chaiaurcode";
+
+let chaiFlavour :string = "masala chai";
+chaiFlavour = "ginger tea";
+
+let chaiOrder:number =90;
+//type annotation ? (:number)
+// type annotation is a way to explicitly specify the type of a variable in TypeScript. It helps in ensuring that the variable holds values of the specified type, providing better type safety and reducing potential runtime errors. In this case, `chaiOrder` is declared as a number, meaning it can hold any numeric value.
